13079fe9f13856c212d75da73c786db4e1dc86af
iamsiddheshwar <sidddofficial18@gmail.com.com>
2026-05-14 05:42:20 +0000

    day-1

---
+++ day-1/main.tf
@@ -0,0 +1,9 @@
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "mybucket" {
+  bucket = var.mybucket
+}
+
---
+++ day-1/variables.tf
@@ -0,0 +1,5 @@
+variable "mybucket" {
+  type    = string
+  default = "mum-hyd-del-05-05-2620256"
+  
+}
